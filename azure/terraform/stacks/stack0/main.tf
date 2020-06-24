9b2f24251afe9a0f58a6f1828e425721a1a6de4d
samitkumarpatel <samitkumarpatel@gmail.com>
2020-06-24 14:55:08 +0200

    added some more resources in stacks/stack0

--- azure/terraform/stacks/stack0/main.tf
+++ azure/terraform/stacks/stack0/main.tf
@@ -57,15 +57,111 @@ resource "azurerm_app_service" "example" {
   
 }
 
-output "appservice_outbound_ip" {
-    value   = azurerm_app_service.example.outbound_ip_addresses 
+resource "azurerm_app_service_slot" "example" {
+  name                = "ass01azwe001${azurerm_resource_group.example.name}"
+  app_service_name    = azurerm_app_service.example.name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+
+  site_config {
+    linux_fx_version  =   "JAVA|8-jre8"
+  }
+
+  app_settings = {
+    "ENV"           = "DEV"
+    "REDIS_URI"     = azurerm_redis_cache.example.hostname
+    "REDIS_POST"    = azurerm_redis_cache.example.ssl_port
+  }
+  
+  identity  {
+      type  =  "SystemAssigned"
+  }
+
 }
 
+
+output "appservice_slot_identity" {
+    value   = azurerm_app_service_slot.example.identity 
+}
+
+output "appservice_identity" {
+    value   = azurerm_app_service.example.identity 
+}
+
+locals { 
+  outbound_ips =  split(",", azurerm_app_service.example.outbound_ip_addresses)
+}
+
+
 resource "azurerm_redis_firewall_rule" "example" {
-  for_each            = toset(split(",",azurerm_app_service.example.outbound_ip_addresses))
-  name                = "rule_${replace(each.key, ".", "_")}"
+  count               = length(local.outbound_ips)
+  name                = "rule_${count.index}"
   redis_cache_name    = azurerm_redis_cache.example.name
   resource_group_name = azurerm_resource_group.example.name
-  start_ip            = each.value
-  end_ip              = each.value
+  start_ip            = element(local.outbound_ips,count.index)
+  end_ip              = element(local.outbound_ips,count.index)
+  depends_on = [
+    azurerm_app_service.example
+  ]
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "example" {
+  name                        = "kvazwe001${azurerm_resource_group.example.name}"
+  location                    = azurerm_resource_group.example.location
+  resource_group_name         = azurerm_resource_group.example.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "get", "list"
+    ]
+
+    secret_permissions = [
+      "get", "list"
+    ]
+
+    storage_permissions = [
+      "get", "list"
+    ]
+  }
+}
+
+resource "azurerm_key_vault_access_policy" "example0" {
+  key_vault_id = azurerm_key_vault.example.id
+
+  tenant_id = azurerm_app_service.example.identity[0].tenant_id
+  object_id = azurerm_app_service.example.identity[0].principal_id
+
+  key_permissions = [
+    "get", "list"
+  ]
+
+  secret_permissions = [
+    "get", "list"
+  ]
+}
+
+resource "azurerm_key_vault_access_policy" "example1" {
+  key_vault_id = azurerm_key_vault.example.id
+
+  tenant_id = azurerm_app_service_slot.example.identity[0].tenant_id
+  object_id = azurerm_app_service_slot.example.identity[0].principal_id
+
+  key_permissions = [
+    "get", "list"
+  ]
+
+  secret_permissions = [
+    "get", "list"
+  ]
 }
